f3958e8e832a1e4a7c87071b5cdee849a01aa58a
DevanFarmer <devanf21@gmail.com>
2023-01-28 20:55:28 +0200

    Created the InputManager Script

---
+++ Exterminate Prototype/Assets/Scripts/InputManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    private PlayerInput playerInput;
+    private PlayerInput.OnFootActions onFoot;
+
+
+    void Awake()
+    {
+        playerInput = new PlayerInput();
+        onFoot = playerInput.OnFoot;
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        onFoot.Enable();
+    }
+
+    private void OnDisable()
+    {
+        onFoot.Disable();
+    }
+}
